c0a739d06735bca57ace1842c3643ec3808b4d7b
Joaquin Fuentes <joaquin.fuentes1327@gmail.com>
2023-06-01 20:32:21 -0300

    agregue header y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
 import "./App.css"
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import {Container} from "react-bootstrap"
 
 const App = () => {
   return (
-    <div>
-        Hola mundo
-    </div>
+    <>
+     <Header></Header>
+     <Container className="mainPage my-5 border p-0">
+    
+     </Container>
+     <Footer></Footer>
+    </>
   );
 };
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className='text-center bg-dark text-light p-3 m-0'>Clima</h1>
+        </header>
+    );
+};
+
+export default Header;
